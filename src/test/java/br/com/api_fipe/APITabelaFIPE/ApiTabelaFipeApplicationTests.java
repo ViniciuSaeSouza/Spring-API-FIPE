322b34ae15f36cd61b398b39ef8caee74c0d1470
Vinicius Saes <viniciusaesouza@gmail.com>
2025-02-15 22:46:45 -0300

    init commit

---
+++ src/test/java/br/com/api_fipe/APITabelaFIPE/ApiTabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_fipe.APITabelaFIPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
